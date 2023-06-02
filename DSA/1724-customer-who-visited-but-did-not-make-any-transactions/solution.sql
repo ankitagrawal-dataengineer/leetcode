82e9be125f6eeb35c954528194d8930ea5837b0f
579ankit <53573220+579ankit@users.noreply.github.com>
2023-06-02 10:15:18 +0000

    [LeetCode Sync] Runtime - 2791 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ DSA/1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,3 @@
+select v.customer_id,count(v.customer_id) as count_no_trans from visits v left join transactions t on v.visit_id=t.visit_id WHERE t.transaction_id IS NULL  group by v.customer_id;
+
+
