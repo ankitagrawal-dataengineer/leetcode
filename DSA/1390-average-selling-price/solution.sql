6f73d8c359e8c3dd86d8754bd9fb119500d4fdbd
579ankit <53573220+579ankit@users.noreply.github.com>
2023-06-02 06:03:09 +0000

    [LeetCode Sync] Runtime - 2191 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ DSA/1390-average-selling-price/solution.sql
@@ -0,0 +1,5 @@
+select p.product_id, ROUND(SUM(p.price * u.units) / SUM(units), 2) as average_price
+from Prices p join UnitsSold u
+on p.product_id = u.product_id
+and u.purchase_date between p.start_date and p.end_date
+group by u.product_id
