dc6f1dc7cf112380ae68ed00e600e33c91f72067
579ankit <53573220+579ankit@users.noreply.github.com>
2023-06-21 14:50:22 +0000

    [LeetCode Sync] Runtime - 2691 ms (5.03%), Memory - 0.0B (100.00%)

--- DSA/1415-students-and-examinations/solution.sql
+++ DSA/1415-students-and-examinations/solution.sql
@@ -1,15 +1,4 @@
+select s1.student_id,s1.student_name,s2.subject_name,count(e.subject_name) as attended_exams from students s1 join subjects s2 left join examinations e on s1.student_id=e.student_id and s2.subject_name=e.subject_name group by s1.student_id,s2.subject_name order by s1.student_id,s2.subject_name
 
 
-Select 
-  s.student_id, 
-  s.student_name,
-  s1.subject_name,
-  count(e.subject_name) as attended_exams
-from Students s
-join Subjects s1
-left join Examinations e
-on s.student_id = e.student_id 
-  and s1.subject_name = e.subject_name
-group by s.student_id, s1.subject_name
-order by s.student_id, s1.subject_name;
 
