81040a65962ca3b6229ccf70d064a747db184882
579ankit <53573220+579ankit@users.noreply.github.com>
2023-07-15 04:38:01 +0000

    [LeetCode Sync] Runtime - 1083 ms (6.94%), Memory - 0.0B (100.00%)

---
+++ DSA/1292-immediate-food-delivery-ii/solution.sql
@@ -0,0 +1,6 @@
+select round(sum(if (x.drnk=1 and x.order_date=x.customer_pref_delivery_date,1,0)*100)/count(distinct customer_id),2) as immediate_percentage 
+from (
+      select * ,dense_rank() over( partition by customer_id order by order_date) as drnk
+      from delivery) x
+           
+          
