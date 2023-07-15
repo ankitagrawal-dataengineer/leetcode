b146d3caa5860c3391fdd650e332931d70e2c1d7
579ankit <53573220+579ankit@users.noreply.github.com>
2023-07-15 04:33:40 +0000

    [LeetCode Sync] Runtime - 2137 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ DSA/2057-count-salary-categories/solution.sql
@@ -0,0 +1,13 @@
+select category, accounts_count from (
+    select (
+        case when income < 20000 then 'Low Salary'
+        when income > 50000 then 'High Salary'
+        else 'Average Salary' end
+    ) as category, count(*) as accounts_count
+    from accounts
+    group by category
+    union (select 'Low Salary', 0)
+    union (select 'Average Salary', 0)
+    union (select 'High Salary', 0)
+) t
+group by category
