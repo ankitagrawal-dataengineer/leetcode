bba682d7b2994e57f74ab714a75f23eac68e3da5
579ankit <53573220+579ankit@users.noreply.github.com>
2023-09-26 10:32:34 +0000

    [LeetCode Sync] Runtime - 1041 ms (5.04%), Memory - 0.0B (100.00%)

--- DSA/0585-investments-in-2016/solution.sql
+++ DSA/0585-investments-in-2016/solution.sql
@@ -4,3 +4,4 @@ WHERE tiv_2015 IN (SELECT tiv_2015 FROM insurance i2
                    WHERE i1.pid != i2.pid)
 AND (lat, lon) NOT IN (SELECT lat, lon FROM insurance i3
                        WHERE i3.pid != i1.pid)
+
