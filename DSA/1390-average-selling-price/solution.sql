30a7e4d1949b9393fe21e96d9cff7428219c3c43
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-19 05:27:04 +0000

    [LeetCode Sync] Runtime - 1506 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/1390-average-selling-price/solution.sql
+++ DSA/1390-average-selling-price/solution.sql
@@ -1,4 +1,1 @@
-select p.product_id,round(sum(p.price*u.units)/sum(u.units),2) as average_price from prices p join unitssold u on p.product_id=u.product_id and u.purchase_date between p.start_date and p.end_date group by u.product_id 
-
-
-
+Select  p.product_id, ifnull(round(sum(u.units * p.price)/sum(u.units),2),0) as average_price  from Prices p left join UnitsSold u on u.purchase_date >= p.start_date and u.purchase_date <= p.end_date and p.product_id = u.product_id group by p.product_id;
