fb04126538237b931b19f104c06d381b8a4b72c8
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-20 07:26:27 +0000

    [LeetCode Sync] Runtime - 1411 ms (5.03%), Memory - 0.0B (100.00%)

--- DSA/1892-find-total-time-spent-by-each-employee/solution.sql
+++ DSA/1892-find-total-time-spent-by-each-employee/solution.sql
@@ -1,3 +1,1 @@
-select event_day as day,emp_id,sum(out_time-in_time) total_time from employees group by emp_id,event_day
-
-
+select event_day as day,emp_id,sum(out_time-in_time) as total_time from employees group by emp_id,event_day
