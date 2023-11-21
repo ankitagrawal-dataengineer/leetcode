f2e7db3e8696353ef665819e2f4ac9cd8cc41e64
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-21 04:56:36 +0000

    [LeetCode Sync] Runtime - 1306 ms (5.03%), Memory - 0.0B (100.00%)

---
+++ DSA/0184-department-highest-salary/solution.sql
@@ -0,0 +1,9 @@
+SELECT t1.Department, t1.Employee, t1.Salary
+FROM (SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+,DENSE_RANK() OVER(PARTITION BY d.id ORDER BY salary DESC) AS rk
+FROM Department AS d
+JOIN Employee AS e ON E.departmentId = d.id) AS t1
+WHERE rk = 1
+
+
+
