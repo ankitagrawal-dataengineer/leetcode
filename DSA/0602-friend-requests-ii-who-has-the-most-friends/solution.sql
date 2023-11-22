96522bf73760ec0c7581f96156a9e08a31e7a2d7
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-22 04:30:24 +0000

    [LeetCode Sync] Runtime - 441 ms (6.83%), Memory - 0.0B (100.00%)

--- DSA/0602-friend-requests-ii-who-has-the-most-friends/solution.sql
+++ DSA/0602-friend-requests-ii-who-has-the-most-friends/solution.sql
@@ -1,7 +1,12 @@
-# Write your MySQL query statement below
-select a as id, count(b) as num from
-(select requester_id  as a, accepter_id  as b from RequestAccepted 
-union
-select accepter_id  as a, requester_id  as b from RequestAccepted ) as T
-group by a
-order by num desc limit 1;
+with cte as
+(
+    select requester_id as id from RequestAccepted
+    union all
+    select accepter_id from RequestAccepted
+)
+
+select id,count(*) as num
+from cte
+group by id
+order by num desc 
+limit 1
