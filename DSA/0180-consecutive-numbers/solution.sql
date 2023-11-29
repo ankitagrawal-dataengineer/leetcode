cd39bd9cd1040d1dc1dfff84ff73698419cca5c8
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-29 03:35:57 +0000

    [LeetCode Sync] Runtime - 1097 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/0180-consecutive-numbers/solution.sql
+++ DSA/0180-consecutive-numbers/solution.sql
@@ -1,7 +1,6 @@
 select distinct i1.num as ConsecutiveNums
 from logs i1, logs i2, logs i3
-where
-i1.id = i2.id + 1 AND
-i2.id = i3.id + 1 AND
-i1.num = i2.num AND
+where i1.id = i2.id+1 and
+i2.id = i3.id+1 and
+i1.num = i2.num and
 i2.num = i3.num
