efea52de406bd32613fc4d3cc7488ebb17ce4bb7
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-29 06:50:29 +0000

    [LeetCode Sync] Runtime - 927 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/0619-biggest-single-number/solution.sql
+++ DSA/0619-biggest-single-number/solution.sql
@@ -1,3 +1,3 @@
-select ifnull((select num from mynumbers group by num having count(num)=1 order by num desc limit 1),null) as num
-
-
+select ifnull((select num from mynumbers
+group by num having count(num)=1 
+order by num desc limit 1),null) as num
