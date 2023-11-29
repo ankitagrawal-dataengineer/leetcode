36fb6087635f96ef53b213c424c25fc83ee442b5
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-29 05:09:23 +0000

    [LeetCode Sync] Runtime - 1314 ms (22.27%), Memory - 0.0B (100.00%)

--- DSA/0185-department-top-three-salaries/solution.sql
+++ DSA/0185-department-top-three-salaries/solution.sql
@@ -1,9 +1,6 @@
-
-
-SELECT Department, employee, salary FROM (
-    SELECT d.name AS Department
-        , e.name AS employee
-        , e.salary
-        , DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS drk
-    FROM Employee e JOIN Department d ON e.DepartmentId= d.Id
-) t WHERE t.drk <= 3
+SELECT t1.Department, t1.Employee, t1.Salary
+FROM (SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary,
+DENSE_RANK() OVER(PARTITION BY d.id ORDER BY salary DESC) AS rk
+FROM Department AS d
+JOIN Employee AS e ON E.departmentId = d.id) AS t1
+WHERE rk<=3
