de8a991995899e24e55482242c28b63de70638de
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-29 03:58:11 +0000

    [LeetCode Sync] Runtime - 1018 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/0183-customers-who-never-order/solution.sql
+++ DSA/0183-customers-who-never-order/solution.sql
@@ -1,3 +1,3 @@
-select name as Customers from customers
-where id not in 
-(select customerid from orders)
+select name as Customers 
+from customers where id not in
+(select customerId from orders)
