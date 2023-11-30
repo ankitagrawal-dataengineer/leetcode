49bec39735b691dd38da23b8a3eda84879415f9c
579ankit <53573220+579ankit@users.noreply.github.com>
2023-11-30 13:36:56 +0000

    [LeetCode Sync] Runtime - 2000 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/1480-movie-rating/solution.sql
+++ DSA/1480-movie-rating/solution.sql
@@ -1,26 +1,25 @@
 with cte1 as
 (
-    select u.name,dense_rank() 
+    select u.name,dense_rank()
     over(order by count(m.user_id) desc,u.name asc) rnk1
     from users u join movierating m
     using(user_id)
-    group by m.user_id
+    group by m.user_id 
 ),
 
 cte2 as
 (
-    select m1.title,dense_rank() 
+    select m1.title,dense_rank()
     over(order by avg(m2.rating) desc,m1.title asc) rnk2
     from movies m1 join movierating m2
     using(movie_id)
     where m2.created_at between '2020-02-01' and '2020-02-29'
     group by m2.movie_id
-    
-    
 )
 
 select name as results from cte1 where rnk1=1
 union all
 select title as results from cte2 where rnk2=1
+
 
 
