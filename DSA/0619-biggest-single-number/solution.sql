cfbb8f58b1ab4e56c2bc831e6da6224c1b883ff9
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-11 12:42:24 +0000

    [LeetCode Sync] Runtime - 760 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/0619-biggest-single-number/solution.sql
+++ DSA/0619-biggest-single-number/solution.sql
@@ -1,5 +1,3 @@
-select ifnull((select num from mynumbers 
-group by num 
-having count(1)=1
-order by num desc
-limit 1),null) as num;
+select max(num) as num from mynumbers where num in
+(select num from mynumbers group by num
+having count(*)=1);
