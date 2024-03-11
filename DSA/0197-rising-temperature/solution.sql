ebd3c83de3e39ce919f6fcccc743bfaf8b8e3554
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-11 11:39:44 +0000

    [LeetCode Sync] Runtime - 936 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/0197-rising-temperature/solution.sql
+++ DSA/0197-rising-temperature/solution.sql
@@ -1,5 +1,4 @@
-select w2.id Id from weather w1 
-join weather w2
-where
-w2.temperature > w1.temperature and
-datediff(w2.recordDate,w1.recordDate)=1;
+select w1.id Id from weather w1 join
+weather w2 where datediff(w1.recordDate,
+w2.recordDate)=1 
+and w1.temperature>w2.temperature;
