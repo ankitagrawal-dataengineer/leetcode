83b71b87d6f866404c25b4c9e549be4f05e270cf
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-12 05:22:13 +0000

    [LeetCode Sync] Runtime - 982 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/1245-user-activity-for-the-past-30-days-i/solution.sql
+++ DSA/1245-user-activity-for-the-past-30-days-i/solution.sql
@@ -1,1 +1,6 @@
-select activity_date as day,count(distinct user_id) as active_users from activity where activity_date between '2019-06-28' and '2019-07-28' group by activity_date
+select activity_date day,count(distinct user_id)
+active_users from activity
+where activity_date between '2019-06-28' 
+and '2019-07-28'
+group by activity_date;
+
