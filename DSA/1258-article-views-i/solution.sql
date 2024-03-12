868be8d151a745ee6543f1069723900d73ec2511
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-12 05:37:08 +0000

    [LeetCode Sync] Runtime - 838 ms (5.02%), Memory - 0.0B (100.00%)

--- DSA/1258-article-views-i/solution.sql
+++ DSA/1258-article-views-i/solution.sql
@@ -1,3 +1,3 @@
-select author_id as id from views
-where author_id=viewer_id group by
-author_id order by id
+select distinct author_id id 
+from views where author_id=viewer_id
+group by author_id order by author_id;
