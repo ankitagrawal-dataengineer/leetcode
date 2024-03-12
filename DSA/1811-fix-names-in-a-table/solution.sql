4049f08d98258797296edd38d0cf9e6a0cbae53d
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-12 12:59:13 +0000

    [LeetCode Sync] Runtime - 1459 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/1811-fix-names-in-a-table/solution.sql
+++ DSA/1811-fix-names-in-a-table/solution.sql
@@ -1,1 +1,3 @@
-select user_id,concat(upper(left(name,1)),'',lower(right(name,length(name)-1))) as name from users order by user_id
+select user_id,concat(upper(left(name,1)),
+'',lower(right(name,length(name)-1))) name 
+from users order by user_id
