794f1a8d6e2076dfbc0fe53021667b33c2b71047
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-13 04:54:02 +0000

    [LeetCode Sync] Runtime - 1386 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/1882-the-number-of-employees-which-report-to-each-employee/solution.sql
+++ DSA/1882-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -1,1 +1,6 @@
-select a1.employee_id,a1.name,count(a2.reports_to) as reports_count,round(avg(a2.age),0) as average_age from employees a1 join employees a2 on a1.employee_id = a2.reports_to group by a2.reports_to order by a1.employee_id
+
+select a1.employee_id,a1.name,count(a2.reports_to) as reports_count,
+round(avg(a2.age),0) as average_age from employees a1 join employees a2 
+on a1.employee_id = a2.reports_to group by a2.reports_to 
+order by a1.employee_id
+
