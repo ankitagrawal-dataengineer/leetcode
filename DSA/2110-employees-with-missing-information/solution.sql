a36216ee49de33394c44ce87b151c7415401eb67
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-13 11:55:39 +0000

    [LeetCode Sync] Runtime - 1341 ms (5.02%), Memory - 0.0B (100.00%)

--- DSA/2110-employees-with-missing-information/solution.sql
+++ DSA/2110-employees-with-missing-information/solution.sql
@@ -1,5 +1,8 @@
-select employee_id from employees where 
-employee_id not in (select employee_id from salaries)
+select employee_id from employees
+where employee_id not in
+(select employee_id from salaries)
 union
-select employee_id from salaries where 
-employee_id not in (select employee_id from employees) order by employee_id
+select employee_id from salaries
+where employee_id not in
+(select employee_id from employees)
+order by employee_id
