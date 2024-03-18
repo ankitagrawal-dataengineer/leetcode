b2605ae14b01d43aae6dc396d3d335069016fedf
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-18 09:07:21 +0000

    [LeetCode Sync] Runtime - 1213 ms (5.99%), Memory - 0.0B (100.00%)

--- DSA/1182-game-play-analysis-iv/solution.sql
+++ DSA/1182-game-play-analysis-iv/solution.sql
@@ -1,9 +1,14 @@
 with cte as (
-    select player_id,DATE_SUB(event_date, interval 1 day) = min(event_date) over(partition by player_id) as is_return from activity
+    select player_id,
+    DATE_SUB(event_date,interval 1 day)
+    =min(event_date) 
+    over(partition by player_id) 
+    as is_return from activity
 )
 
-select round(sum(is_return)/count(distinct player_id),2) as fraction from cte
-
+select round(sum(is_return)
+/count(distinct player_id),2) 
+as fraction from cte
 
 
 
