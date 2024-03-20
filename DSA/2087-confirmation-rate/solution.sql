c936c6372ba9d2e516ab4ee134d8a75db0d7054e
579ankit <53573220+579ankit@users.noreply.github.com>
2024-03-20 07:27:46 +0000

    [LeetCode Sync] Runtime - 1028 ms (5.00%), Memory - 0.0B (100.00%)

--- DSA/2087-confirmation-rate/solution.sql
+++ DSA/2087-confirmation-rate/solution.sql
@@ -1,2 +1,7 @@
-select s.user_id,ifnull(round(sum(if(c.action='confirmed',1,0))/count(c.user_id),2),0) confirmation_rate from signups s left join confirmations c on s.user_id=c.user_id group by s.user_id
+select s.user_id,ifnull(round(sum(c.action='confirmed')/
+count(c.user_id),2),0) confirmation_rate from 
+signups s left join confirmations c on
+s.user_id=c.user_id group by s.user_id
+
+
 
