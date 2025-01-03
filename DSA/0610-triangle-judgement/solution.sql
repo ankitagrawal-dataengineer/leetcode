336004a22880b8b40f6b1e5ee76e9ac8aec96e6a
579ankit <53573220+579ankit@users.noreply.github.com>
2025-01-03 10:33:32 +0000

    [LeetCode Sync] Runtime - 794 ms (5.01%), Memory - 0.0B (100.00%)

--- DSA/0610-triangle-judgement/solution.sql
+++ DSA/0610-triangle-judgement/solution.sql
@@ -1,3 +1,6 @@
-select *,
-if(x+y>z and y+z>x and z+x>y,'Yes','No')
-as triangle from triangle;
+select x,y,z,
+case when (x+y>z and y+z>x and x+z>y) then 'Yes' 
+else 'No' end as triangle from triangle
+
+
+
