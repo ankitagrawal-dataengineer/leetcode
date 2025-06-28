f2808aa6a5a531ac7f569a48667553c74d841be7
579ankit <53573220+579ankit@users.noreply.github.com>
2025-06-28 07:42:34 +0000

    [LeetCode Sync] Runtime - 418 ms (66.26%), Memory - 0.0B (100.00%)

--- DSA/0175-combine-two-tables/solution.sql
+++ DSA/0175-combine-two-tables/solution.sql
@@ -1,3 +1,3 @@
-select p.firstName,p.lastName,a.city,a.state 
+select p.firstname,p.lastname,a.city,a.state
 from person p left join address a
-on p.personId = a.personId;
+on p.personid = a.personid;
