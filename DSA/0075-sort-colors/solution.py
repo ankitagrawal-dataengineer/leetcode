cd885626ef466f5ae9ca69595011448aacf42994
579ankit <53573220+579ankit@users.noreply.github.com>
2026-06-26 10:44:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.3 MB (89.79%)

--- DSA/0075-sort-colors/solution.py
+++ DSA/0075-sort-colors/solution.py
@@ -5,17 +5,15 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         n=len(nums)
-        low=0
-        high=n-1
-        mid=0
+        low,mid,high=0,0,n-1
         while mid<=high:
-                if nums[mid]==0:
-                    nums[low],nums[mid]=nums[mid],nums[low]
-                    low+=1
-                    mid+=1
-                elif nums[mid]==1:
-                    mid+=1
-                elif nums[mid]==2:
-                    nums[high],nums[mid]=nums[mid],nums[high]
-                    high-=1
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            elif nums[mid]==2:
+                nums[high],nums[mid]=nums[mid],nums[high]
+                high-=1   
         return nums
