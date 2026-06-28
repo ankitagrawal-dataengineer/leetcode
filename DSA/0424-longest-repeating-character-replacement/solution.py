e8404c7210b618b6ac171ab3de6c43e7af0d296e
579ankit <53573220+579ankit@users.noreply.github.com>
2026-06-28 04:50:24 +0000

    [LeetCode Sync] Runtime - 139 ms (68.10%), Memory - 15.4 MB (59.38%)

---
+++ DSA/0424-longest-repeating-character-replacement/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        n=len(s)
+        low=0
+        freq={}
+        res=0
+        max_count=0
+        for high in range(n):
+            freq[s[high]] = freq.get(s[high], 0) + 1
+            length=high-low+1
+            max_count=max(max_count,freq[s[high]])
+            diff=length-max_count
+
+            if diff>k:
+                freq[s[low]]-=1
+                low+=1
+
+            length=high-low+1
+            res=max(res,length)
+        return res
