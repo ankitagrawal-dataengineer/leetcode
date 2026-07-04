d59c49e98171110e7cef9051d1ad5c095131f4ce
579ankit <53573220+579ankit@users.noreply.github.com>
2026-07-04 02:31:53 +0000

    [LeetCode Sync] Runtime - 7 ms (50.64%), Memory - 12.9 MB (43.72%)

--- DSA/0167-two-sum-ii---input-array-is-sorted/solution.py
+++ DSA/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -9,9 +9,9 @@ class Solution(object):
         i,j=0,n-1
         while i<j:
             s=numbers[i]+numbers[j]
-            if s>target:
-                j-=1
-            elif s==target:
-                return [i+1,j+1]
-            else:
+            if s<target:
                 i+=1
+            elif s>target:
+                j-=1
+            else:
+                return [i+1,j+1]
