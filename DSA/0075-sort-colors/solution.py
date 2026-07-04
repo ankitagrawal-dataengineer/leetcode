977bbf7332fac132d624517b13d4915c13a9f209
579ankit <53573220+579ankit@users.noreply.github.com>
2026-07-04 04:47:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (17.92%)

--- DSA/0075-sort-colors/solution.py
+++ DSA/0075-sort-colors/solution.py
@@ -10,12 +10,14 @@ class Solution(object):
         high=n-1
         while mid<=high:
             if nums[mid]==0:
-                nums[mid],nums[low]=nums[low],nums[mid]
-                low+=1
+                nums[low],nums[mid]=nums[mid],nums[low]
                 mid+=1
+                low+=1
             elif nums[mid]==1:
                 mid+=1
             elif nums[mid]==2:
-                nums[mid],nums[high]=nums[high],nums[mid]
+                nums[high],nums[mid]=nums[mid],nums[high]
                 high-=1
         return nums
+
+
