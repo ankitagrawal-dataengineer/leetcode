528efb15728b2fc2e64ba07f65f953fb68d4813c
579ankit <53573220+579ankit@users.noreply.github.com>
2026-07-06 04:34:17 +0000

    [LeetCode Sync] Runtime - 803 ms (5.91%), Memory - 15.8 MB (21.58%)

--- DSA/0076-minimum-window-substring/solution.py
+++ DSA/0076-minimum-window-substring/solution.py
@@ -1,25 +1,25 @@
 class Solution(object):
     def bool_sahi(self,need,have):
         for ch in need:
-            if have.get(ch,0)<need[ch]:
+            if need[ch]>have.get(ch,0):
                 return False
         return True
+
     def minWindow(self, s, t):
-        if len(s)< len(t):
+        if len(s)<len(t):
             return ""
-        need={}
         have={}
+        need={}
+        low,n=0,len(s)
         res=float('inf')
         start=-1
         for ch in t:
             need[ch]=need.get(ch,0)+1
-        low=0
-        n=len(s)
         for high in range(n):
             have[s[high]]=have.get(s[high],0)+1
             while self.bool_sahi(need,have):
                 length=high-low+1
-                if length<res:
+                if res>length:
                     res=length
                     start=low
                 have[s[low]]-=1
@@ -28,3 +28,5 @@ class Solution(object):
             return ""
         else:
             return s[start:start+res]
+
+
