c35751a8a10b8a2a975c885df907885732e90e6f
579ankit <53573220+579ankit@users.noreply.github.com>
2026-07-06 11:29:17 +0000

    [LeetCode Sync] Runtime - 955 ms (76.78%), Memory - 0.0B (100.00%)

---
+++ DSA/1773-percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,5 @@
+select r.contest_id,round(count(r.contest_id)*100/(select count(*) from users),2) as percentage
+from users u join register r
+on u.user_id=r.user_id
+group by r.contest_id
+order by percentage desc,r.contest_id;
