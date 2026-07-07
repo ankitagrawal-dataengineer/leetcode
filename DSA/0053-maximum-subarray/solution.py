df43d9bdb1683a70dd530c3756bafbd3134fee50
579ankit <53573220+579ankit@users.noreply.github.com>
2026-07-07 08:23:01 +0000

    [LeetCode Sync] Runtime - 110 ms (6.05%), Memory - 21.2 MB (45.61%)

--- DSA/0053-maximum-subarray/solution.py
+++ DSA/0053-maximum-subarray/solution.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ans=best_ending=nums[0]
+        best_ending=ans=nums[0]
         for i in range(1,len(nums)):
-            v1=nums[i]
-            v2=best_ending+nums[i]
+            v1=best_ending+nums[i]
+            v2=nums[i]
             best_ending=max(v1,v2)
             ans=max(ans,best_ending)
         return ans
