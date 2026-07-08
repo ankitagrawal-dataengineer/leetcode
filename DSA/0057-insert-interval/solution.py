acf4e465a5a81da7cb4e425b80f47ce792f6304e
579ankit <53573220+579ankit@users.noreply.github.com>
2026-07-08 04:09:57 +0000

    [LeetCode Sync] Runtime - 3 ms (57.54%), Memory - 14.2 MB (52.69%)

--- DSA/0057-insert-interval/solution.py
+++ DSA/0057-insert-interval/solution.py
@@ -5,27 +5,27 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
-        arr = []
-        inserted = False
+        inserted=False
+        arr=[]
         for interval in intervals:
-            if not inserted and newInterval[0] < interval[0]:
+            if inserted==False and newInterval[0]<interval[0]:
                 arr.append(newInterval)
-                inserted = True
+                inserted=True
             arr.append(interval)
-        if not inserted:
+        if inserted==False:
             arr.append(newInterval)
-        res = []
-        start1 = arr[0][0]
-        end1 = arr[0][1]
-        for i in range(1, len(arr)):
-            start2 = arr[i][0]
-            end2 = arr[i][1]
-            if end1 >= start2:
-                end1 = max(end1, end2)
+            
+        start1=arr[0][0]
+        end1=arr[0][1]
+        res=[]
+        for i in range(1,len(arr)):
+            start2=arr[i][0]
+            end2=arr[i][1]
+            if end1>=start2:
+                end1=max(end1,end2)
             else:
-                res.append([start1, end1])
-                start1 = start2
-                end1 = end2
-        res.append([start1, end1])
+                res.append([start1,end1])
+                start1=start2
+                end1=end2
+        res.append([start1,end1])
         return res
-        
