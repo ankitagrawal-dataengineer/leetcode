bb82eaf4447596b278783e4b700379785106413c
579ankit <53573220+579ankit@users.noreply.github.com>
2026-07-08 03:54:43 +0000

    [LeetCode Sync] Runtime - 8 ms (83.07%), Memory - 17.3 MB (8.95%)

--- DSA/0056-merge-intervals/solution.py
+++ DSA/0056-merge-intervals/solution.py
@@ -5,26 +5,18 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         intervals.sort()
-        res = []
-        start1 = intervals[0][0]
-        end1 = intervals[0][1]
-
-        for i in range(1, len(intervals)):
-            start2 = intervals[i][0]
-            end2 = intervals[i][1]
-
-            if end1 >= start2:
-                #start1=start1
+        start1=intervals[0][0]
+        end1=intervals[0][1]
+        res=[]
+        for i in range(len(intervals)):
+            start2=intervals[i][0]
+            end2=intervals[i][1]
+            if end1>=start2:
                 end1=max(end1,end2)
                 continue
             else:
-                res.append([start1, end1])
-                start1 = start2
-                end1 = end2
-        res.append([start1, end1])
+                res.append([start1,end1])
+                start1=start2
+                end1=end2
+        res.append([start1,end1])
         return res
-
-
-
-
-
